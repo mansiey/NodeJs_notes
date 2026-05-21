bb3cb69d07deddc4dc1abe5cd532006e93b8c42d
mansiey <mansi46rd@gmail.com>
2026-05-21 14:00:52 +0530

    learn modules in NodeJS

---
+++ Day-1/hello.js
@@ -0,0 +1,12 @@
+console.log("Hello, from JS world!")
+
+console.log(typeof window);
+
+
+
+// Modules
+
+const math = require('./math');
+
+console.log('Math value is:', math.add(5, 3));
+console.log('Math value is:', math.sub(5, 3));
---
+++ Day-1/math.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    return a+b;
+}
+
+function sub(a, b){
+    return a-b;
+}
+
+// module.exports = add;
+// module.exports = sub;
+
+
+module.exports = {
+    add,
+    sub
+}
